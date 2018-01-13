50958fb88958871c4b2ff72ed35c17b5d3e1e9dc
yellappam <yellappa.madigonde@foodstuffs.co.nz>
2018-01-13 05:28:25 +0000

    Step 11: Assign Multiple Targets

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -14,6 +14,10 @@ sap.ui.define([
 
 		onNavToEmployees: function(oEvent) {
 			this.getRouter().navTo("employeeList");
+		},
+
+		onNavToEmployeeOverview: function(oEvent) {
+			this.getRouter().navTo("employeeOverview");
 		}
 
 	});
